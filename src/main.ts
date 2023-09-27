49b9e52301e75a4aadac0035a05b2d16826af928
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2023-09-27 22:56:46 +0200

    new compilation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Product, ProductList } from "../src/classes";
 
 let productList = new ProductList();
 
-productList.addProduct(new Product("Floor Lamp name", 109.99, "An electric light supported by a tall pole with a base on the floor.", "img/1_floorLamp.png"))
+productList.addProduct(new Product("Floor Lamp name", 109.99, "An electric light supported by a tall pole with a base on the floor.", "img/13_floorLamp.jpeg"))
 productList.addProduct(new Product("Floor Lamp name", 109.99, "An electric light supported by a tall pole with a base on the floor.", "img/2_floorLamp.jpeg"))
 productList.addProduct(new Product("Floor Lamp name", 109.99, "An electric light supported by a tall pole with a base on the floor.", "img/3_floorLamp.jpeg"))
 productList.addProduct(new Product("Floor Lamp name", 109.99, "An electric light supported by a tall pole with a base on the floor.", "img/4_floorLamp.jpeg"))
